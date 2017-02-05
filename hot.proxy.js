f3be81c49f6d2181b0e9d7aac7c60956868d9ffc
Joel Noyce Barnham <joelnbarnham@gmail.com>
2017-02-05 19:24:10 +0000

    Enable linting .js files which are not stored in the client directory & fix some lint errors shown on those files

--- hot.proxy.js
+++ hot.proxy.js
@@ -28,12 +28,12 @@ app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output
 app.use(webpackHotMiddleware(compiler));
 app.use(proxy('http://localhost:' + port));
 
-port++
+port++;
 
 app.listen(port, function(error) {
   if (error) {
     console.error(error);
   } else {
-    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port);
+    console.info('==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port);
   }
 });
