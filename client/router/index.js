99b3dc21b031228c4f00ca773f5deffb854e671a
comerc <comerc@users.noreply.github.com>
2017-05-30 01:19:15 +0300

    typo

--- client/router/index.js
+++ client/router/index.js
@@ -28,7 +28,7 @@ export function run() {
 }
 
 // Export it to render on the Golang server, keep the name sync with -
-// https://github.com/olebedev/go-starter-kit/blob/master/src/app/server/react.go#L65
+// https://github.com/olebedev/go-starter-kit/blob/master/server/react.go#L65
 export const renderToString = toString;
 
 require('../css');
